3eea905dcd5b73b36043888d7295c1ad35c77551
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-01 20:00:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  a = array.slice(0, array.length - 1).join(", ")
+  b = ", and "
+  c = array.slice(-1)
+  
+  case array.length
+    when 0 
+      return []
+    when 1 
+      return array[0]
+    when 2 
+      return array[0] + " and " + array[1]
+    else
+      return a + b + c 
+    end
 end
