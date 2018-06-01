575e141bfb72b83262fde94618c7f6085ab1e48b
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-01 19:58:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,13 @@
 def oxford_comma(array)
-  if array.length == 2 
-    return array[0] + array[1]
-  elsif 
+  case array.length
+    when 0 
+      return array[0] + array[1]
+    when 1 
+      return array[0]
+    when 2 
+      return 
+  elsif array.length == 0 
+    return
   a = array.slice(0, array.length - 1).join(", ")
   b = ", and "
   c = array.slice(-1)
