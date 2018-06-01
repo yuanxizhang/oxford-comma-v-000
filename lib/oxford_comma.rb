3ff9489c827fa68b896debfa1a6359975e1ebff1
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-01 19:53:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,6 @@
 def oxford_comma(array)
   a = array.slice(0, array.length - 1).join(", ")
-  b = ", and"
+  b = ", and "
   c = array.slice(-1)
-  puts a
-  puts b
-  puts c
   return a + b + c 
 end
